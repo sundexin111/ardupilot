f90c0c97d47418bdc5b8fc60e93a7eb07ef934ff
sundexin111 <sundexin00@gmail.com>
2020-04-28 22:35:36 +0800

    copter阶段完成

--- ArduCopter/mode_loiter.cpp
+++ ArduCopter/mode_loiter.cpp
@@ -103,6 +103,7 @@ void Copter::ModeLoiter::run()
 
         // get pilot desired climb rate
         target_climb_rate = get_pilot_desired_climb_rate(channel_throttle->get_control_in());
+        //爬升速率的限幅 细节
         target_climb_rate = constrain_float(target_climb_rate, -get_pilot_speed_dn(), g.pilot_speed_up);
     } else {
         // clear out pilot desired acceleration in case radio failsafe event occurs and we do not switch to RTL for some reason
